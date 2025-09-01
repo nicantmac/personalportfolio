a6909ee50a9d0ac66b5c8a3d251258b5d5c491ee
Nicholas Mack <nam7nam14@gmail.com>
2025-09-01 06:31:04 -0400

    Changed Lorem ipsum text and added description for most recent project.

--- src/components/bottomLayout/ProjectCards.js
+++ src/components/bottomLayout/ProjectCards.js
@@ -3,7 +3,10 @@ const cardData = [
         title: "LearnFlip",
         image: "",
         status: "Jun 2024",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.",
+        description: "LearnFlip is an interactive learning platform with modern designed to " +
+            "promising learning to be comprehensive and welcoming to all knowledge background. " +
+            "Help student learners master STEM and technical subjects through dynamic card-based learning." +
+            " Inspired by the simplicity of Quizlet and the interactivity of Brilliant.org",
     },
     {
         title: "AI-Powered Front Door Cam",
