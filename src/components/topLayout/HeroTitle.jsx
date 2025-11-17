13905db85e2e7734021d3ba989c3d5dce26281e5
Nicholas Mack <nam7nam14@gmail.com>
2025-11-16 22:56:51 -0500

    updated Hero Title ui

--- src/components/topLayout/HeroTitle.jsx
+++ src/components/topLayout/HeroTitle.jsx
@@ -11,17 +11,21 @@ function HeroTitle() {
     // noinspection JSValidateTypes
     return (
         <section className="hero-container"> {/* hero entire container */}
-            <div className="hero-main"> {/* hero inner container */}
+            <main className="hero-main"> {/* hero inner container */}
 
                 {/* Intro Text */}
-                <h1 className="hero-intro">{t('hero.title')}<span className="wave" role="img" aria-label="wave">👋🏼</span></h1>
+                <h1 className="hero-intro">
+                    {t('hero.title')}<span className="wave" role="img" aria-label="wave">👋🏼</span>
+                </h1>
                 <h2 className="hero-name">{t('hero.name')}</h2>
                 <p className="hero-subtitle">{t('hero.subtitle')}</p>
 
                 {/* Buttons */}
-                <div className="hero-buttons">
-                    <button className="hero-title-btns">{t('hero-btn.talk')}<img src={peopleIcon} alt="people icon" /></button>
-                    <a href="#about" className="hero-title-btns" aria-label="Scroll Down">{t('hero-btn.explore')}<img src={downArrowIcon} alt="down arrow" /></a>
+                <div className="hero-btns">
+                    <button className="talk-btn" aria-label="bio section">{t('hero-btn.talk')}<img src={peopleIcon} alt="people icon" /></button>
+                    <a href="#about" className="explore-btn" aria-label="scroll down btn">
+                        {t('hero-btn.explore')}<img src={downArrowIcon} alt="down arrow" />
+                    </a>
                 </div>
 
                 {/* Social Links */}
@@ -30,7 +34,7 @@ function HeroTitle() {
                     <a href="https://www.linkedin.com/in/nicholasmack972/" target="_blank" className="icon linkedin" aria-label="LinkedIn"></a>
                     <a href="https://github.com/nicantmac?tab=repositories" target="_blank" className="icon github" aria-label="GitHub"></a>
                 </div>
-            </div>
+            </main>
         </section>
     );
 }
