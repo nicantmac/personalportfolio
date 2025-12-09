b8e4745a19d42f25f3d511edf5750cf951944e8e
Nicholas Mack <nam7nam14@gmail.com>
2025-12-09 17:51:36 -0500

    added newer img of wht one of the projects looks today and updated project data with new projects started and completed

--- src/components/bottomLayout/ProjectCards.js
+++ src/components/bottomLayout/ProjectCards.js
@@ -7,7 +7,7 @@ const cardData = [
         desc: "LearnFlip is an interactive learning platform with modern designed to " +
             "promising learning to be comprehensive and welcoming to all knowledge background. " +
             "Help student learners master STEM and technical subjects through dynamic card-based learning." +
-            " Inspired by the simplicity of Quizlet and the interactivity of Brilliant.org",
+            " Inspired by the simplicity of Quizlet and the interactivity of Brilliant.org"
     },
     {
         title: "AI-Powered Front Door Cam",
@@ -17,7 +17,7 @@ const cardData = [
         desc: "AI-powered front door camera using a Raspberry Pi Module 4 and Google Coral USB Edge TPU " +
             "for real-time object detection. Used a night vision (NoIR camera module) and PIR motion sensors to " +
             "detect visitors and classify activity with minimal latency. The project demonstrates my exploration" +
-            " in edge AI integration and embedded systems.",
+            " in edge AI integration and embedded systems."
     },
     {
         title: "ImagineAI",
@@ -27,15 +27,25 @@ const cardData = [
         desc: "CNN model classifies 700+ images across multiple flower species. " +
             "The project involved data preprocessing, augmentation, and deep learning model optimization " +
             "to achieve high classification accuracy. Built using TensorFlow/Keras and Python, this project" +
-            " demonstrates skills in computer vision, neural network architecture design, and image recognition.",
+            " demonstrates skills in computer vision, neural network architecture design, and image recognition."
+    },
+    {
+        title: "GhostGuarded",
+        image: "",
+        status: "In-progress",
+        link: "https://github.com/nicantmac",
+        desc: "A platform that people can use to add there job application-process details and the system " +
+            "predicts and outputs the likeliness of being ghosted by a company. If ghosted, the system uses " +
+            "ML model to take corresponding data i.e. situation, company details, your info etc to " +
+            "construct a written complaint to the Better Business Bereau"
     },
     {
         title: "Reconstructing",
         image: "",
         status: "TBD",
         link: "",
-        desc: "Coming Soon...",
-    },
+        desc: "Coming Soon..."
+    }
 ];
 
 export default cardData;
