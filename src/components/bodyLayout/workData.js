bce9a122b75337bf2451b9fef2a7e3e493878046
Nicholas Mack <nam7nam14@gmail.com>
2026-03-01 19:36:48 -0500

    Added new wrk history and css update

--- src/components/bodyLayout/workData.js
+++ src/components/bodyLayout/workData.js
@@ -1,18 +1,19 @@
 export const workItems = [
     {
-        id: "learnflip",
-        logo: "",
-        org: "LearnFlip",
-        role: "Developer",
-        date: "September 2025 - Present",
-        tools: ["Typescript", "React.js", "Framer Motion", "Figma", "Web Accessibility", " Express.js", "JWT", "Supabase", "Docker"],
+        id: "todd",
+        logo: "public/orgs/toddagriscience_logo.jpeg",
+        org: "Todd AgriScience",
+        role: "Frontend Developer Intern",
+        date: "February 2026 - Present",
+        tools: ["Typescript", "Next.js", "Framer Motion", "Figma", "Web Accessibility", " Express.js", "Supabase", "Vitest", "Tailwind"],
         listing: [
-            "React.js learning platform to help students learn 100+ STEM topics via flashcards & fill-in blanks.",
-            "Uses Open Trivia API to dynamically load question sets by topic and difficulty.",
-            "Ensuring responsive UX/UI with Framer Motion and Web Accessibility.",
-            "Developed an Express.js backend with secure RESTful API routes, and session " +
-            "handling using JWT tokens and using Supabase Auth & data management features.",
-            "Using Docker to package the full-stack system in a container and using AWS EC2 to decide where and how the container runs.",
+            "Developed and owned the Disclosures page in a Next.js application, delivering a " +
+            "responsive and accessible UI.",
+            "Implemented dynamic PDF-based content and internationalized routing (EN/ES) within an " +
+            "established localization structure",
+            "Collaborated in an Agile team with daily standups, scoped tasks, deadlines, and GitHub " +
+            "pull-request reviews",
+            "Used Next.js, Tailwind CSS, Framer Motion, Vitest and Git to ship production-ready frontend features"
         ]
     },
     {
@@ -22,9 +23,11 @@ export const workItems = [
         role: "Student Intern",
         date: "July 2025 - Present",
         tools: ["PII", "Security Clearance", "Handling federal documents"],
-        listing: ["Helped with federal onboarding and assisted with administrative tasks required for DOJ access",
+        listing: [
+            "Helped with federal onboarding and assisted with administrative tasks required for DOJ access",
             "Organized case files, prepared documentation packets, and maintained accurate records for attorney review",
-            "Completed mandatory federal compliance trainings on ethics, privacy, security, and records handling"]
+            "Completed mandatory federal compliance trainings on ethics, privacy, security, and records handling"
+        ]
     },
     {
         id: "resilience",
@@ -33,9 +36,14 @@ export const workItems = [
         role: "AI Software Developer Intern",
         date: "August 2024 - December 2024",
         tools: ["Python", "Git/Github", "Firebase", "MFCC's", "Librosa", "Tensorflow", "Pandas", "Numpy"],
-        listing: ["Strengthened authentication and user-management workflows by gaining hands-on experience with secure login, session handling, and role-based access",
-            "Ensured safe and compliant data storage by implementing best practices for client-side and server-side security, including encryption and validation",
-            "Collaborated with senior software engineers to troubleshoot issues, optimize performance, and contribute to cross-team projects across multiple divisions at Resilience Inc"]
+        listing: [
+            "Strengthened authentication and user-management workflows by gaining hands-on experience with secure " +
+            "login, session handling, and role-based access",
+            "Ensured safe and compliant data storage by implementing best practices for client-side and server-side " +
+            "security, including encryption and validation",
+            "Collaborated with senior software engineers to troubleshoot issues, optimize performance, and contribute " +
+            "to cross-team projects across multiple divisions at Resilience Inc"
+        ]
     },
     {
         id: "headstarter",
@@ -44,9 +52,14 @@ export const workItems = [
         role: "Full-Stack Student Mentee",
         date: "May 2024 - September 2024",
         tools: ["Javascript", "Typescript", "RAG", "OpenAI", "Next.js", "React", "Node.js", "Pinecone", "Stripe API", "AWS", "Git/Github"],
-        listing: ["Developed an AI-powered customer support system using OpenAI, Next.js, and AWS, enabling automated responses and enhanced user assistance",
-            "Built an AI Flashcards application integrated with OpenAI, authentication services, and Stripe API for secure payments and user access control",
-            "Contributed to an AI-driven Rate-My-Professor–style platform using Retrieval-Augmented Generation (RAG), OpenAI models, and vector-search technologies to deliver context-aware professor insights"]
+        listing: [
+            "Developed an AI-powered customer support system using OpenAI, Next.js, and AWS, enabling " +
+            "automated responses and enhanced user assistance",
+            "Built an AI Flashcards application integrated with OpenAI, authentication services, and Stripe API " +
+            "for secure payments and user access control",
+            "Contributed to an AI-driven Rate-My-Professor–style platform using Retrieval-Augmented Generation (RAG), OpenAI " +
+            "models, and vector-search technologies to deliver context-aware professor insights"
+        ]
     },
     {
         id: "ai",
@@ -55,9 +68,13 @@ export const workItems = [
         role: "AI/ML Student Mentee",
         date: "August 2023 - December 2023",
         tools: ["Python", "Numpy", "Pandas", "Tensorflow", "Matplotlib", "Text-to-Speech"],
-        listing: ["Developed a Convolutional Neural Network (CNN) trained on a dataset of 700+ flower species using supervised learning techniques.",
-            "Performed data preprocessing, normalization, and augmentation to improve model generalization and accuracy.",
+        listing: [
+            "Developed a Convolutional Neural Network (CNN) trained on a dataset of 700+ flower species " +
+            "using supervised learning techniques.",
+            "Performed data preprocessing, normalization, and augmentation to improve model generalization " +
+            "and accuracy.",
             "Achieved over 85% accuracy on the final trained model for multi-class classification",
-            "Implemented a text-to-speech feature to read predictions aloud, enhancing accessibility and user interaction"]
+            "Implemented a text-to-speech feature to read predictions aloud, enhancing accessibility and user interaction"
+        ]
     }
 ]
